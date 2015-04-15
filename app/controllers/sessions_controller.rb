c384ab4e381028c2e48d3a1a87e64d7061a55e9e
Debbie Gillespie <debbieg@umn.edu>
2015-04-15 15:29:59 -0500

    Implements searching with query_string_search
    Provides a way to filter set of sessions using institution_id, term_id,
    and academic_career_id.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
   def index
-    @sessions = Session.all
+    sessions = Session.all
+
+    searchable_sessions = SearchableSessions.new(sessions)
+
+    query_string_search = params[:q]
+    @sessions = QueryStringSearch.new(searchable_sessions, query_string_search).results
 
     render
   end
---
+++ app/models/searchable_sessions.rb
@@ -0,0 +1,31 @@
+class SearchableSessions
+  extend Forwardable
+  def_delegators :all_sessions, :each, :select
+
+  attr_accessor :all_sessions
+
+  def initialize(all_sessions)
+    self.all_sessions = all_sessions.map {|c| SearchableSession.new(c) }
+  end
+end
+
+
+class SearchableSession < SimpleDelegator
+
+  def institution_id
+    @institution ||= session.institution.institution_id
+  end
+
+  def term_id
+    @term ||= session.term.term_id
+  end
+
+  def academic_career_id
+    @academic_career ||= session.academic_career.academic_career_id
+  end
+
+  def session
+    __getobj__
+  end
+
+end
