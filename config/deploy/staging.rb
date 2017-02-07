7cf73aa81946c417ab2b2c4cea2afecf5f1c8d89
Debbie Gillespie <debbieg@umn.edu>
2017-02-07 14:37:19 -0600

    Update the server names for application is hosted
    We are moving this site to a rhel7 server.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,4 @@
-role :app, %w{asr-sessions-stage1.oit.umn.edu}
+set :rails_env,  "staging"
 
-set :server, 'asr-sessions-stage1.oit.umn.edu'
-
-server 'asr-sessions-stage1.oit.umn.edu',
-  roles: fetch(:roles),
-  web_root: fetch(:web_root),
-  ssh_options: {
-    user: fetch(:user),
-    forward_agent: true,
-    auth_methods: %w(publickey)}
+server "asr-sessions-qat-02.oit.umn.edu",
+       roles: %w(web app db prod)
