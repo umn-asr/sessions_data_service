f9ae979176cf295e2b6c47efcff16120c0d12bc8
Brittany Greaner <grea0022@umn.edu>
2020-01-13 14:45:20 -0600

    Update rack-cors and other tooling
    **Locked `rack-cors` to version 1.0.6**
    
    When we deployed to staging, the server gave us this error: `Incomplete
    response received from application`. Running the tests, we noticed the
    tests were giving us the following errors: `undefined method
    'unescape_path' for Rack::Utils:Module`.
    
    [We found this issue](https://github.com/cyu/rack-cors/issues/191) which said that
    1.0.6 version fixes these errors. After locking to 1.0.6 all the tests
    pass.
    
    **Added Oracle Cleaner**
    
    We noticed there were TFMS database tables in our development and test
    databases. To eliminate this as a possibility for troubleshooting, we
    introduced Oracle Cleaner to the project.
    
    **Updated `db/schema.rb` with boilerplate**
    
    This will keep showing as a change in Git if we don't commit it as is.
    As it's just boilerplate, we thought it would be safe to add.
    
    Co-authored-by: Brittany Greaner <grea0022@umn.edu>
    Co-authored-by: Tony Thomas <thoma127@umn.edu>

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,16 @@
-# ActiveRecord expects schema.rb to exist.  We don't need a schema.rb file because 
-# we are simply reading from the asr_warehouse, so there is no schema to manage.
-# Adding this blank file makes rake stop complaining when running our tests.
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 0) do
+
+end
