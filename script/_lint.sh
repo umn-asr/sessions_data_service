563a2fcaafbcb32ee1154218afae549d92ae9bda
Tony Thomas <thoma127@umn.edu>
2023-10-20 08:27:48 -0500

    Remove Overcommit and add brakeman and standard
    I've temporarily disabled Standard and ignore CVEs, but I've added a
    message in the script so we can stop ignoring those once we upgrade
    Rails to a patched version.
    
    Standard is also being skipped. I'll address the standard findings in a
    separate commit.

--- script/_lint.sh
+++ script/_lint.sh
@@ -7,22 +7,29 @@ set -e
 
 cd "$(dirname "$0")/.."
 
+RED="\033[0;31m"
+NC='\033[0m' # No Color
+
 if [ "$LINT_ALL" = "true" ]; then
   git config --global http.proxy http://kraken02.oit.umn.edu:3128
   git config --global https.proxy https://kraken02.oit.umn.edu:3128
   echo "==> Checking Gems..."
+  echo "==> ${RED}NOTE: Several CVES are being ignored in .bundler-audit.yml. Remove them once we've upgraded Rails.${NC}"
   bundle audit check --update
   echo "==> Running Brakeman..."
   brakeman
   echo "==> Running Reek..."
   reek
-  echo "==> TODO: Install and configure standard gem..."
+  echo "==> ${RED}Skipping Standard for now...${NC}"
   # echo "==> Running Standard..."
   # standardrb --no-fix
 else
+  echo "==> Checking Gems..."
+  echo "==> ${RED}NOTE: Several CVES are being ignored in .bundler-audit.yml. Remove them once we've upgraded Rails.${NC}"
+  bundle audit check --update
   echo "==> Running Reek..."
   reek $(git diff --name-only --ignore-submodules=all --cached) --force-exclusion
-  echo "==> TODO: Install and configure standard gem..."
+  echo "==> ${RED}Skipping Standard for now...${NC}"
   # echo "==> Running Standard..."
   # standardrb --no-fix
 fi
