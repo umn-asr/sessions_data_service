50dccddf10bab73ff3a24d362821a65969dc4582
Tony Thomas <thoma127@umn.edu>
2023-10-26 13:04:04 -0500

    Remove warning about ignored CVEs
    The file to ignore CVEs was removed in #63.

--- script/_lint.sh
+++ script/_lint.sh
@@ -7,14 +7,10 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-RED="\033[0;31m"
-NC='\033[0m' # No Color
-
 if [ "$LINT_ALL" = "true" ]; then
   git config --global http.proxy http://kraken02.oit.umn.edu:3128
   git config --global https.proxy https://kraken02.oit.umn.edu:3128
   echo "==> Checking Gems..."
-  echo "==> ${RED}NOTE: Several CVES are being ignored in .bundler-audit.yml. Remove them once we've upgraded Rails.${NC}"
   bundle audit check --update
   echo "==> Running Brakeman..."
   brakeman
@@ -24,7 +20,6 @@ if [ "$LINT_ALL" = "true" ]; then
   standardrb --no-fix
 else
   echo "==> Checking Gems..."
-  echo "==> ${RED}NOTE: Several CVES are being ignored in .bundler-audit.yml. Remove them once we've upgraded Rails.${NC}"
   bundle audit check --update
   echo "==> Running Reek..."
   reek $(git diff --name-only --ignore-submodules=all --cached) --force-exclusion
